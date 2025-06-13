53431949fa908a3fb88a330a69509ba904cd8289
MrKitey <60539960+TekiDev42@users.noreply.github.com>
2025-06-13 16:53:18 +0200

    Update AutoclickerFrame.py

--- classes/AutoclickerFrame.py
+++ classes/AutoclickerFrame.py
@@ -1,10 +1,14 @@
 import customtkinter as ctk
 
+# Custom class that inherits from CTkFrame to create frames in the interface
 class AutoclickerFrame(ctk.CTkFrame):
+    # Default transparent background color
     default_fg_color="transparent"
     
     def __init__(self, master, **kwargs):
+        # If no background color is specified, use the default color
         if "fg_color" not in kwargs:
             kwargs["fg_color"] = self.default_fg_color
 
+        # Call the parent constructor with parameters
         super().__init__(master, **kwargs)
